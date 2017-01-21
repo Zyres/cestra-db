0e6328f94afcb146f2832e67fdaf6db64ed64829
the-broccoli <non@non.non>
2017-01-21 16:53:59 +0100

    fightpos fixed in astrub city (sub area: 95)

---
+++ updates/0003_fightpos_astrub_city.sql
@@ -0,0 +1,42 @@
+-- ----------------------------
+-- FIGHTPOS - Astrub (Astrub City)
+-- by Cestra https://github.com/Cestra
+-- ----------------------------
+UPDATE `maps` SET `places`="dbdqc0dsc2bvcpfZ|dMe9gfguf8gAg4gy" WHERE `id`=7344;
+UPDATE `maps` SET `places`="dTd8d7ekejeyexeM|gfgugtgIgHgWgVg-" WHERE `id`=7360;
+UPDATE `maps` SET `places`="cvcKcJcYcXdac_do|flfAfzfOfNf2f1ge" WHERE `id`=7392;
+UPDATE `maps` SET `places`="bubJbYcbbZbKbvbg|heg1gMgxgjgygNg2" WHERE `id`=7424;
+UPDATE `maps` SET `places`="|" WHERE `id`=7550;
+UPDATE `maps` SET `places`="aGaUaFaTa8a7bkby|gugJgXgIgWg_hng-" WHERE `id`=7345;
+UPDATE `maps` SET `places`="fWf-fVgngBgmf9gA|cEcScDc7djc6cRdi" WHERE `id`=7409;
+UPDATE `maps` SET `places`="gcgrgdf0fMf1fNfy|aVa9a-blbmbzbAbO" WHERE `id`=7425;
+UPDATE `maps` SET `places`="dQdCdodadpdDdRd5|hlg8gUg9gVgGgsgH" WHERE `id`=7441;
+UPDATE `maps` SET `places`="dqdDdQd2ecd1dOdC|cccpcCcOc0cNcAco" WHERE `id`=7614;
+UPDATE `maps` SET `places`="cmcPcAb9clb8czcO|fTf8gkfUf9glgzgA" WHERE `id`=7410;
+UPDATE `maps` SET `places`="ceb1bNb2bObzblbA|gef1fNf2fOfzflfA" WHERE `id`=7442;
+UPDATE `maps` SET `places`="dydJdzdKdAdpdqdg|b4cdcccncxcmcwcH" WHERE `id`=7615;
+UPDATE `maps` SET `places`="cVc6dec5dodddydn|b4cdcncccmcxcHcw" WHERE `id`=7606;
+UPDATE `maps` SET `places`="d8elekezeNeyeMe1|ftfIfuffe3fge4eP" WHERE `id`=7553;
+UPDATE `maps` SET `places`="eAeOfffufvfheQeB|e9flgdgreYfbf9gm" WHERE `id`=10111;
+UPDATE `maps` SET `places`="fOfzfNfyf1gef2gf|cvcucIcfbObPb4bQ" WHERE `id`=7427;
+UPDATE `maps` SET `places`="f1gdgtgHfOfAgUg8|ceb2b3bPbma-blbz" WHERE `id`=7443;
+UPDATE `maps` SET `places`="eReDeoeCend_dWd-|d8elekezeyeNeMe1" WHERE `id`=7551;
+UPDATE `maps` SET `places`="fifxfwfLfKfZfYgb|d9emeleAezeOeNe2" WHERE `id`=12262;
+UPDATE `maps` SET `places`="fifxfwfLfKfZfYgb|d9emeleAezeOeNe2" WHERE `id`=7511;
+UPDATE `maps` SET `places`="fMf0gcgqgbfZfLfw|eBemeAePe3eOe2ff" WHERE `id`=7501;
+UPDATE `maps` SET `places`="dmdndAc2d1eneyd-|dqdPdpdNebezdzdy" WHERE `id`=7596;
+UPDATE `maps` SET `places`="fKfYfJgafXfIftfe|eAemeBeQeCeRend-" WHERE `id`=7572;
+UPDATE `maps` SET `places`="|" WHERE `id`=7574;
+UPDATE `maps` SET `places`="eBemeAePeOe3e2ff|fMfxfLf0fZgcgbgq" WHERE `id`=7514;
+UPDATE `maps` SET `places`="|" WHERE `id`=7529;
+UPDATE `maps` SET `places`="eVe8dXdydadddDdmc2|d0ebenezemd_dZdN" WHERE `id`=7546;
+UPDATE `maps` SET `places`="|" WHERE `id`=7549;
+UPDATE `maps` SET `places`="|" WHERE `id`=7544;
+UPDATE `maps` SET `places`="e1eNeze2fffufgft|dHdVd-dWd_eneCeD" WHERE `id`=7581;
+UPDATE `maps` SET `places`="epeDeSeE|cicwcLcx" WHERE `id`=7528;
+UPDATE `maps` SET `places`="c1cRc2cScHcxcIcy|ebd3ecd4dTdJdUdK" WHERE `id`=7527;
+UPDATE `maps` SET `places`="cBb-b_cCc5c6cDca|cxb6bDbCb5cwcvb4" WHERE `id`=7431;
+UPDATE `maps` SET `places`="f0gcgqgEgbfZfKfY|ftffe2eOe3ePeAem" WHERE `id`=7515;
+UPDATE `maps` SET `places`="fkfygff3cEcTc7cSaZbcbqbb|dEdSdTd7gbgpgqgEgMg1glf8" WHERE `id`=7416;
+UPDATE `maps` SET `places`="c5difwele1fffuga|f-fVecdYdJc3dHeS" WHERE `id`=12095;  
+UPDATE `maps` SET `places`="dMdAdcdpdOd1enez|eAeMeXeKeyeldLdz" WHERE `id`=7760;
