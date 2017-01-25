62435ca2f56ca3e9f5201d5e89f5d981623f4287
The-Broccoli <korepp.f@gmail.com>
2017-01-25 23:31:24 +0100

    fightpos fixed in astrub and tainela (sub area: 173,278,30)

--- updates/0002_fightpos_astrub_area.sql
+++ updates/0002_fightpos_astrub_area.sql
@@ -1,6 +1,9 @@
 -- ----------------------------
--- FIGHTPOS - Astrub (Astrub City) 
+-- FIGHTPOS for Astrub,Tainela
 -- by Cestra https://github.com/Cestra
+-- ----------------------------
+-- Astrub (Astrub City) 
+-- Sub area:95
 -- ----------------------------
 UPDATE `maps` SET `places`="dbdqc0dsc2bvcpfZ|dMe9gfguf8gAg4gy" WHERE `id`=7344;
 UPDATE `maps` SET `places`="dTd8d7ekejeyexeM|gfgugtgIgHgWgVg-" WHERE `id`=7360;
@@ -42,6 +45,7 @@ UPDATE `maps` SET `places`="c5difwele1fffuga|f-fVecdYdJc3dHeS" WHERE `id`=12095;
 UPDATE `maps` SET `places`="dMdAdcdpdOd1enez|eAeMeXeKeyeldLdz" WHERE `id`=7760;
 -- ----------------------------
 -- Astrub (Surroundings of Astrub)
+-- Sub area:92
 -- ----------------------------
 UPDATE `maps` SET `places`="bFbrbYbJeRe6eJeu|cBcQg7g6fde1bNce" WHERE `id`=7448;
 UPDATE `maps` SET `places`="dfclbrcjbRb3dKeD|e5fjfZffemd8dadb" WHERE `id`=7449;
@@ -56,6 +60,7 @@ UPDATE `maps` SET `places`="cDcocCcnb_bWb-bV|e3ePeAeOezfge4fh" WHERE `id`=7313;
 UPDATE `maps` SET `places`="eTe7fjfxfMfyfke8|dcdrdqdFdEdTdSd7" WHERE `id`=7311;
 -- ----------------------------
 -- Astrub (Tofus Corner)
+-- Sub area:101
 -- ----------------------------
 UPDATE `maps` SET `places`="dIdfcBdsddcyd9el|fMf0fjeTe8fKeDeC" WHERE `id`=7765;
 UPDATE `maps` SET `places`="ehewcZcLbda1aTa8|cpcbfAf3f_fXeadX" WHERE `id`=7767;
@@ -75,6 +80,7 @@ UPDATE `maps` SET `places`="dIdVeCe4fJd7dbc0|b_cQcPd-fHfUeLez" WHERE `id`=7776;
 UPDATE `maps` SET `places`="dWemeOfef0gqfZgr|cib5bRbSbpchcKcJ" WHERE `id`=7777;
 -- ----------------------------
 -- Astrub (Astrub Mines)
+-- Sub area:96
 -- ----------------------------
 UPDATE `maps` SET `places`="dVd9eld8eAe2eNey|cAc4dudfc2dtdHcP" WHERE `id`=7599;
 UPDATE `maps` SET `places`="e0fde1eMeyeNezek|cAcOc3dfc2dedt" WHERE `id`=7600;
@@ -84,6 +90,7 @@ UPDATE `maps` SET `places`="dF|dUel" WHERE `id`=7556;
 UPDATE `maps` SET `places`="e0fse2eyd6dCfYfu|cOdgdIeaeEdecMci" WHERE `id`=7605;
 -- ----------------------------
 -- Astrub (Astrub Rocky Inlet)
+-- Sub area:335
 -- ----------------------------
 UPDATE `maps` SET `places`="emeBe3eOezeyfgfJdWd_|dXdIdgdvdjdic5cQcBb-" WHERE `id`=10167;
 UPDATE `maps` SET `places`="bOd5dFeJgRgEf0gtfg|ctejfpg4fZgdeAfu" WHERE `id`=10168;
@@ -94,6 +101,7 @@ UPDATE `maps` SET `places`="gFgqgbgpgagogCfv|bEbTb8cObUb9bVbH" WHERE `id`=10188;
 UPDATE `maps` SET `places`="cBb_bHb9bFbdaLbb|egdQeieKfceMfefG" WHERE `id`=10181;
 -- ----------------------------
 -- Astrub (Astrub Fields)
+-- Sub area:98
 -- ----------------------------
 UPDATE `maps` SET `places`="cWcXcucvb4b5bCbD|eSfjfkfNfOgfgggJ" WHERE `id`=7402;
 UPDATE `maps` SET `places`="cXcYb4bCbbbccibS|flfjgdgqgRgneSfw" WHERE `id`=7403;
@@ -101,6 +109,7 @@ UPDATE `maps` SET `places`="cgcfbObAbmbPbQbn|flfkfOf2fNgegdfM" WHERE `id`=7405;
 UPDATE `maps` SET `places`="fWf-gnf_fgfvd8eM|eydUfufJeQe5eReC" WHERE `id`=7613;
 -- ----------------------------
 -- Astrub (Astrub Forest)
+-- Sub area:97
 -- ----------------------------
 UPDATE `maps` SET `places`="f8glfwfLa2beaYaZ|ecdZcQcCc-dnc_cX" WHERE `id`=8705;
 UPDATE `maps` SET `places`="f2gegsgGcXcJcvch|gkgzgOcRcCcnd_e3" WHERE `id`=8706;
@@ -115,3 +124,39 @@ UPDATE `maps` SET `places`="eDeSe6eRe5fifwfh|bTbFbrbqbUb8b9bG" WHERE `id`=7356;
 UPDATE `maps` SET `places`="fvfKfYfZfLfwfhfi|cQc4dhcPc3dgdvdu" WHERE `id`=7357;
 UPDATE `maps` SET `places`="dpddc2dccpcBcocN|dDdPd2dQepeBeNeZ" WHERE `id`=8708;
 UPDATE `maps` SET `places`="fKgbgagEgDgCf_gB|cyb6b7bDbEb8bFbb" WHERE `id`=7340;
+-- ----------------------------
+-- Astrub (Astrub Meadows)
+-- Sub area:173
+-- ----------------------------
+UPDATE `maps` SET `places`="dndQeheKeidRdPeg|d3evd4dBdCd5ewd6" WHERE `id`=10166;
+UPDATE `maps` SET `places`="fTglgBfXgogpgqf0fj|djcEcpbYbvbra1bea_bnaV" WHERE `id`=8570;
+UPDATE `maps` SET `places`="fPggfOgfgtgWgVgs|bVbsbtbWb_cCcDca" WHERE `id`=8582;
+UPDATE `maps` SET `places`="gQgAfFfbcgceb2bza-|bca2bYcEeHe9fNfygHgsgc" WHERE `id`=8567;
+UPDATE `maps` SET `places`="fMfyfke9fAfOgfgegV|eYevdQejaZa1brbH" WHERE `id`=8610;
+UPDATE `maps` SET `places`="eZfEfFf8fVgBfRgygz|b4bmbRbpbrbVbHbuay" WHERE `id`=8607;
+UPDATE `maps` SET `places`="b7c2dJeEe7fNf2gI|b3cudnfpf7gAf-fI" WHERE `id`=8564;
+UPDATE `maps` SET `places`="cBcQc6djbWbHfeft|cDcoe0fdfufJgpgE" WHERE `id`=8599;
+UPDATE `maps` SET `places`="dudJdvdgc4dhc5cQ|dXd_eneBePe3ffft" WHERE `id`=8562;
+UPDATE `maps` SET `places`="blbzbNdTekglgAgP|dfdueodKdZe8fkgb" WHERE `id`=8574;
+UPDATE `maps` SET `places`="bVb-cxcZcvb5dQdBa8aU|bWbHcicKd4dPaFaT" WHERE `id`=8573;
+UPDATE `maps` SET `places`="edeseGe9flfzf2gdgc|cWctbObAa9aVaWaX" WHERE `id`=8604;
+UPDATE `maps` SET `places`="bXcacpcogIgHgVgUgF|eLewd5eibna-aWa_bo" WHERE `id`=8598;
+UPDATE `maps` SET `places`="bYbJbGbVgfgtfUf9cJcv|aTcVbnbpcMcObecQece9fye3fwgFd8eL" WHERE `id`=8597;
+-- ----------------------------
+-- Astrub (Amakna Castle Gobball Breeding Station)
+-- Sub area:278
+-- ----------------------------
+UPDATE `maps` SET `places`="cKdcdadpdEdTdCd6|cjcicxcMc1dedsddc0cLcwcZdrdGdVd-eBemd9dUdFdqdbcYd8eleAePe3eOezekd7dSdDdodRejeyeN" WHERE `id`=7805;
+UPDATE `maps` SET `places`="fKfvfge3eOezekd7|dSdDd6ejeyeNe2fffufJfYfwfhe4ePeAeld8dTdEdpdqdFdUd9emeBeQe5fie6eReCend-dVdGdrdccZcLc0dddsdHdWd_eoeDeSeEepeadXdIdtc1cMcxcjcycNc2cOc3dgdudJdvdKdYebeqdZ" WHERE `id`=7796;
+UPDATE `maps` SET `places`="dDdodadpdbcYcKcZ|eMexejeyekd7dTd8dEdScJcX" WHERE `id`=7799;
+UPDATE `maps` SET `places`="c0dddrdcdqdFdTdE|dSd7ekd8dUdGc1cMdbcZcLdVd9elezeOeAemd-eneBePe3fge4eQeCeoeaepeDeRe5fhfvfKfwfie6eSeE" WHERE `id`=7800;
+UPDATE `maps` SET `places`="bobSckcOdUdqcYcu|eAeld8dTdEdpdacXcvcgcKcZdcdrdGdVd-dWdHdsddc0cLcwchb4bQb5cicxcMc1dedtdIdudfc2cNcycjb6bRbCbDb7czc3dgc4cPcAclb8emd9dFdbcJ" WHERE `id`=7801;
+UPDATE `maps` SET `places`="dvckdIc0emdbd8dD|dYdJdudfc3c2cNcycjb7cMc1dedXeadWdHdsddcZdcdrdGdVd-d9dUdFdqdpdEdTeleAeOezekd7dSd6ejeyeNe2" WHERE `id`=7802;
+-- ----------------------------
+-- Tainela (The Cradle)
+-- Sub area:30
+-- ----------------------------
+UPDATE `maps` SET `places`="bgbIbHb_b-cDbdcA|fkf1f0e6eQdWdVci" WHERE `id`=8550;
+UPDATE `maps` SET `places`="e-fme9flfAfOf3gg|gugfgIgXgJhag_gY" WHERE `id`=1868;
+UPDATE `maps` SET `places`="c2dec3dvepdGemeD|feeMcicKb-cCeTfj" WHERE `id`=1869;
+UPDATE `maps` SET `places`="e9eGeddMdjcScpbY|dPdmcVcec-csb1by" WHERE `id`=1860;
