be266990901d627aec0cf95c7938c29e62ce1c95
The-Broccoli <korepp.f@gmail.com>
2017-02-07 18:15:10 +0100

    fightpos fixed in lousy pif plain  (sub area: 178)

--- updates/0003_fightpos_cania_plains_area.sql
+++ updates/0003_fightpos_cania_plains_area.sql
@@ -36,3 +36,42 @@ UPDATE `maps` SET `places`="fTfVfXeZd5eUdLdh|c3dtc1czb7cxbRbp" WHERE `id`=3588;
 UPDATE `maps` SET `places`="cQdgcOcmdtc1b7bF|cxdFeyfIgagcfjeE" WHERE `id`=3587;
 UPDATE `maps` SET `places`="dTelePe2ddb7ckbD|dEdFfheieKbqb9cO" WHERE `id`=3586;
 UPDATE `maps` SET `places`="eEeadIdgcQcmbUbq|ehfEglgBgofYfwe6" WHERE `id`=3585;
+-- ----------------------------
+-- Cania Plains (Lousy Pif Plain) 
+-- Sub area:178
+-- ----------------------------
+UPDATE `maps` SET `places`="eSe7fjfxfLfwf1fN|b2cec_cWcIcucgcv" WHERE `id`=3624;
+UPDATE `maps` SET `places`="eKeZeLe0fcfrfdfG|bXbIbWbHbtbebsa1" WHERE `id`=3623;
+UPDATE `maps` SET `places`="fEfTfFfrfGfVf9gm|b_bWbIbubfa2bea1" WHERE `id`=3622;
+UPDATE `maps` SET `places`="ejexeLewehd4dQdC|ckb8bUb9cmcBcPc3" WHERE `id`=3621;
+UPDATE `maps` SET `places`="fTf7gkgzglf9fUfF|b-cncBcPcAclb9bV" WHERE `id`=3620;
+UPDATE `maps` SET `places`="aUa9a8blbzbkbybN|fIftfffufge3ePe4" WHERE `id`=3619;
+UPDATE `maps` SET `places`="c3cAb9bUbTcNcybS|ftfee1eMeNezeOe2" WHERE `id`=3618;
+UPDATE `maps` SET `places`="bTbScjcicLcKdbda|eUflfkfNfMgdgcgF" WHERE `id`=3617;
+UPDATE `maps` SET `places`="bNcecfbObmbnaWaV|eReoepeSfhfKfJfg" WHERE `id`=3616;
+UPDATE `maps` SET `places`="a1brbTcjcLdbdDd5|c7dxdZepeRfhfJf_" WHERE `id`=3615;
+UPDATE `maps` SET `places`="dbcYcKcZcLcwcicx|fNfzfMfLfKfie6eSe7" WHERE `id`=3661;
+UPDATE `maps` SET `places`="hogYgIggf2fMgsg-|bza9ctcfcvb5bna_" WHERE `id`=3663;
+UPDATE `maps` SET `places`="blbAbPb3bOb2cfct|gcgrgdgsgGgUgVge" WHERE `id`=3664;
+UPDATE `maps` SET `places`="cfb2bOb3bPbAblbz|eCd_d-eadJdIdfdg" WHERE `id`=3707;
+UPDATE `maps` SET `places`="brbFbqbcaZbbbpbE|eQe4fgffe2eNeMex" WHERE `id`=3706;
+UPDATE `maps` SET `places`="frfUglgkfTfqfpfSgj|cpbYbXbubta2a1ay" WHERE `id`=3705;
+UPDATE `maps` SET `places`="cfb3bObAbmbBbnb4|gFgrgcf0fMf1fNgs" WHERE `id`=3704;
+UPDATE `maps` SET `places`="fLfxfMfje7eTe8eF|bnbBbmbPb3cfb2ct" WHERE `id`=3702;
+UPDATE `maps` SET `places`="fdeNeMe0eLexd6ew|bVbtbIbWb_bXcacC" WHERE `id`=3701;
+UPDATE `maps` SET `places`="cKcLcicjdbdadDdC|eDe5fvfZgrgVgWgu" WHERE `id`=3700;
+UPDATE `maps` SET `places`="cXcYcvcwb5b6bDbE|g8gFgEgbgafJfIff" WHERE `id`=3699;
+UPDATE `maps` SET `places`="eCeDe6e5fvfYfXfu|b3b2ctcubBbCa_a-" WHERE `id`=3698;
+UPDATE `maps` SET `places`="dedIdvc3czb9cBc5|fdfHfUgmgOgkfSfq" WHERE `id`=3697;
+UPDATE `maps` SET `places`="b3cgcJchcKcZdcdr|gcf0gdgsgefye7eS" WHERE `id`=3738;
+UPDATE `maps` SET `places`="cpcabtbebqbccAcm|fTfrfHe1ezePd9dH" WHERE `id`=3739;
+UPDATE `maps` SET `places`="d8eAeQfhfifMgdge|cXcYcvcwb5b6bDbE" WHERE `id`=3740;
+UPDATE `maps` SET `places`="gzf8f7fqfrfGfeeO|a0a1bsbFcBbWbXco" WHERE `id`=3741;
+UPDATE `maps` SET `places`="eUe8fkfyfMf0gcgqgE|cgcucIcWc-bQbCboba" WHERE `id`=3786;
+UPDATE `maps` SET `places`="dEdcc1cxb6b5bCa-|gyf7f8fFfGfeeLeh" WHERE `id`=3785;
+UPDATE `maps` SET `places`="fTglfpfceKegd5ex|bFb8b9cAcBc4c5dw" WHERE `id`=3784;
+UPDATE `maps` SET `places`="f0gsf2fAfwe4d8eA|duc2cybCb4cucWb6" WHERE `id`=3783;
+UPDATE `maps` SET `places`="dpcZcxb5cvcXcfbP|dudJebeqeFfjfLgd" WHERE `id`=3818;
+UPDATE `maps` SET `places`="cZdrdpdSdRcxcyb7|fhfKfieReSepeqd0" WHERE `id`=3819;
+UPDATE `maps` SET `places`="fkfMfje6fifwfLgc|cPcmcOclbUbTckcN" WHERE `id`=3820;
+UPDATE `maps` SET `places`="bUbra0a1bsbVbWbta2|eieLfcfbeKehegeJfa" WHERE `id`=3821;
