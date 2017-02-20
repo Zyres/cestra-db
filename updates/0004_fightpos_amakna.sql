431ae3f80a36d9776b3ade71957cb05b1667623a
The-Broccoli <korepp.f@gmail.com>
2017-02-20 17:46:52 +0100

    fightpos fixed in madrestam harbour (sub area: 1,34)

---
+++ updates/0004_fightpos_amakna.sql
@@ -0,0 +1,29 @@
+-- ----------------------------
+-- FIGHTPOS for Amakna
+-- by Cestra https://github.com/Cestra
+-- ----------------------------
+-- Jail
+-- Sub area:34
+-- ----------------------------
+UPDATE `maps` SET `places`="dWenemeBePeAe3e2|dsdrdUdFdTd8d7ey" WHERE `id`=1003;
+UPDATE `maps` SET `places`="dtdIdWdHdsd_d-dV|cOc3dgdvdudfdXde" WHERE `id`=1002;
+UPDATE `maps` SET `places`="dudYepdXdWeoeRen|fsfWgnfVfUgmgPgl" WHERE `id`=1048;
+-- ----------------------------
+-- Amakna (Madrestam Harbour)
+-- Sub area:1
+-- ----------------------------
+UPDATE `maps` SET `places`="fhfwfLfJgdgrgFgT|e2eweKevf6gjgygN" WHERE `id`=765;
+UPDATE `maps` SET `places`="dXd-eleyeMeAeoec|cQc3dedrdac0cOcC" WHERE `id`=1562;
+UPDATE `maps` SET `places`="eZfcfqfbfpfEfFfU|gmgBg4hhg5gQgCgR" WHERE `id`=9468;
+UPDATE `maps` SET `places`="e7fjfxf2gegFgrgd|eJeYfbfpfEf7gkgz" WHERE `id`=9467;
+UPDATE `maps` SET `places`="gCgRgQg5hhg4gBgm|fUfFfqfceZfbfpfE" WHERE `id`=9662;
+UPDATE `maps` SET `places`="gmgOf8gyfqfoeuew|bGcmbWcCcSdidMed" WHERE `id`=164;
+UPDATE `maps` SET `places`="grgWfYeOdEcuaV|bHcobubJbZcTc7dk" WHERE `id`=0000;
+UPDATE `maps` SET `places`="bcbdbsbebHbVbWbX|fcfFfUfGf9glgmgP" WHERE `id`=165;
+UPDATE `maps` SET `places`="csceb2bOcgcucIcW|d8eleAePffe2eNey" WHERE `id`=926;
+-- ----------------------------
+-- Amakna (Amakna Castle)
+-- Sub area:180
+-- ----------------------------
+UPDATE `maps` SET `places`="c_cWc-dneYfaeXeJ|cHcVgjgygxgMgLg0" WHERE `id`=9466;
+UPDATE `maps` SET `places`="fNfyfkfzgHgVg-gW|gegdf0grgff3guhm" WHERE `id`=9451;
