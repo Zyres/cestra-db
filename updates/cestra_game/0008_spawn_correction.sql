7eb0b485decba6695548ab779dd5ddd31353f294
The-Broccoli <korepp.f@gmail.com>
2017-05-18 18:14:47 +0200

    correction of monster count in sub-area: 335

---
+++ updates/cestra_game/0008_spawn_correction.sql
@@ -0,0 +1,28 @@
+-- ----------------------------
+-- correction of monster count in sub-area: 335
+-- by Cestra https://github.com/Cestra
+-- ----------------------------
+UPDATE `cestra_game`.`maps` SET `monsters`='' WHERE `id`='10167';
+UPDATE `cestra_game`.`maps` SET `minSize`='1', `maxSize`='4' WHERE `id`='10168';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10189';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10186';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10182';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10169';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10170';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10155';
+UPDATE `cestra_game`.`maps` SET `minSize`='1', `maxSize`='4' WHERE `id`='10190';
+UPDATE `cestra_game`.`maps` SET `minSize`='1', `maxSize`='4' WHERE `id`='10187';
+UPDATE `cestra_game`.`maps` SET `minSize`='1', `maxSize`='4' WHERE `id`='10183';
+UPDATE `cestra_game`.`maps` SET `minSize`='1', `maxSize`='4' WHERE `id`='10178';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10177';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10171';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10188';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10184';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10179';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10172';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10185';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10180';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10173';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10181';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10174';
+UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10175';
