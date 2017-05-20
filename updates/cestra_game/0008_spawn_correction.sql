1b1e93a2081b3a67052eb765957314de94ac50a8
The-Broccoli <korepp.f@gmail.com>
2017-05-20 13:56:52 +0200

    spawn correction in sub-area:1

--- updates/cestra_game/0008_spawn_correction.sql
+++ updates/cestra_game/0008_spawn_correction.sql
@@ -1,6 +1,9 @@
 -- ----------------------------
--- correction of monster count in sub-area: 335
+-- SPAWN CORRECTION
 -- by Cestra https://github.com/Cestra
+-- ----------------------------
+-- correction of monster count in
+-- sub-area: 335
 -- ----------------------------
 UPDATE `cestra_game`.`maps` SET `monsters`='' WHERE `id`='10167';
 UPDATE `cestra_game`.`maps` SET `minSize`='1', `maxSize`='4' WHERE `id`='10168';
@@ -26,3 +29,30 @@ UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10173';
 UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10181';
 UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10174';
 UPDATE `cestra_game`.`maps` SET `minSize`='1' WHERE `id`='10175';
+-- ----------------------------
+-- spawn correction
+-- sub-area: 1
+-- ----------------------------
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='174';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='176';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='765';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='763';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='761';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='1040';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='1041';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='1042';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='166';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='167';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='183';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='184';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='908';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='185';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='912';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='914';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='186';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='196';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='902';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='913';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='921';
+UPDATE `cestra_game`.`maps` SET `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='926';
+UPDATE `cestra_game`.`maps` SET `monsters`='|63,5|63,3|63,6|31,3|63,4', `numgroup`='1', `minSize`='1', `maxSize`='4' WHERE `id`='930';
