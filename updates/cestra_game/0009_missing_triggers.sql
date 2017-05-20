ca884678e4d4aacc8a36e68a5b13e93789af6fbb
The-Broccoli <korepp.f@gmail.com>
2017-05-20 14:02:39 +0200

    found missing map trigger fixed

---
+++ updates/cestra_game/0009_missing_triggers.sql
@@ -0,0 +1,23 @@
+-- ----------------------------
+-- MISSING TRIGGERS (all over the maps)
+-- by Cestra https://github.com/Cestra
+-- ----------------------------
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('8586', '456', '0', '1', '8585,36', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('8599', '202', '0', '1', '8605,233', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('8605', '218', '0', '1', '8599,187', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('7432', '173', '0', '1', '7448,233', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('10253', '104', '0', '1', '10253,369', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('10253', '354', '0', '1', '10253,89', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('1561', '290', '0', '1', '761,268', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('761', '254', '0', '1', '1561,278', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('1048', '225', '0', '1', '1002,429', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('1002', '444', '0', '1', '1048,240', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('2016', '342', '0', '1', '2015,238', '-1');
+UPDATE `cestra_game`.`scripted_cells` SET `ActionsArgs`='921,436' WHERE `MapID`='902' and`CellID`='376';
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('921', '450', '0', '1', '902,390', '-1');
+UPDATE `cestra_game`.`scripted_cells` SET `ActionsArgs`='921,438' WHERE `MapID`='926' and`CellID`='19';
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('921', '453', '0', '1', '926,34', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('913', '455', '0', '1', '919,36', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('919', '21', '0', '1', '913,440', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('1562', '296', '0', '1', '765,184', '-1');
+REPLACE INTO`cestra_game`.`scripted_cells` (`MapID`, `CellID`, `ActionID`, `EventID`, `ActionsArgs`, `Conditions`) VALUES ('765', '170', '0', '1', '1562,283', '-1');
