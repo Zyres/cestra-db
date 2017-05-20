afb2a9d10cea6a9d3f53f07555fc5cc27c9721dc
The-Broccoli <korepp.f@gmail.com>
2017-05-20 13:03:27 +0200

    fightpos fixed in madrestam harbour (sub area:1)

---
+++ updates/cestra_game/0010_missing_fightpos.sql
@@ -0,0 +1,21 @@
+-- ----------------------------
+-- MISSING FIGHTPOS
+-- by Cestra https://github.com/Cestra
+-- ----------------------------
+-- Amakna (Madrestam Harbour)
+-- Sub area:1
+-- ----------------------------
+UPDATE `maps` SET `places`="cDcRc6djc7dxdLd0|eZfcfrfde1fFfUf9" WHERE `id`=183;
+UPDATE `maps` SET `places`="gpgSgDg6gRhig5hh|eheieweKeYfaeJev" WHERE `id`=184;
+UPDATE `maps` SET `places`="bDboa_bCbpbaaXaY|fNfzfOfke8flfAe9" WHERE `id`=908;
+UPDATE `maps` SET `places`="ePeAel|d-eneC c" WHERE `id`=2016; -- House of the MAX-LoL (MapID:185)
+UPDATE `maps` SET `places`="dHd-eneC|e4ePeAel" WHERE `id`=2015; -- House of the MAX-LoL (MapID:185)
+UPDATE `maps` SET `places`="gzgOg2gNg1gygMhe|gggugIgXgJhahog_" WHERE `id`=185;
+UPDATE `maps` SET `places`="eYfDf7fUfqfcfdeL|clbTbUbGb-bca1bt" WHERE `id`=912;
+UPDATE `maps` SET `places`="fqfFfUf9gzgkf7fS|b-cncCcRcpcabXbI" WHERE `id`=914;
+UPDATE `maps` SET `places`="d2dQcOc0d9ekeXeL|exeKdNd0c2ddedep" WHERE `id`=6146;
+UPDATE `maps` SET `places`="gzg2gNg1gMgygkgj|cBdJdvdgducQc4cP" WHERE `id`=186;
+UPDATE `maps` SET `places`="kCkVlbkUlaltlLls|jtjMjujbiVjciWiD" WHERE `id`=196;
+UPDATE `maps` SET `places`="bQbPcgcfcIcHc-c9|fkfNfMgdgcgFgEg7" WHERE `id`=902;
+UPDATE `maps` SET `places`="fxe5e6eCeDe7eEea|cjbRbQcicLchcKcg" WHERE `id`=913;
+UPDATE `maps` SET `places`="dKdZdLdwdidxdjc6|bUbVb9clb8ckczcO" WHERE `id`=921;
